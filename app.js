8ca7281ac7b3418ded2a770fcbb232c6fc38c28f
Barrett <baxance@protonmail.com>
2021-01-26 20:52:45 -0800

    redid the code from Monday's lab and made it work and do the counter

--- app.js
+++ app.js
@@ -1,89 +1,152 @@
-let allProducts = [];
-
-function mallProduct(productName, productPath) {
-  this.productName = productName;
-  this.productPath = productPath;
-  this.productShown = 0;
-  this.shownArray= [];
-
-  this.shownArray.push(this.productShown)
-  allProducts.push(this);
-}
-
-
-new mallProduct('robot bag', 'img/bag.jpg');
-new mallProduct('banana slicer', 'img/banana.jpg');
-new mallProduct('ipad bathroom stand', 'img/bathroom.jpg');
-new mallProduct('toeless boots', 'img/boots.jpg');
-new mallProduct('breakfast machine', 'img/breakfast.jpg');
-new mallProduct('meat flavored gum', 'img/bubblegum.jpg');
-new mallProduct('uncomfortable chair', 'img/chair.jpg');
-new mallProduct('cthulhu doll', 'img/cthulhu.jpg');
-new mallProduct('duck mask for dog', 'img/dog-duck.jpg');
-new mallProduct('canned dragon meat', 'img/dragon.jpg');
-new mallProduct('pen utensil attachment', 'img/pen.jpg');
-new mallProduct('pet mop gloves', 'img/pet-sweep.jpg');
-new mallProduct('pizza scissors', 'img/scissors.jpg');
-new mallProduct('shark sleeping bag', 'img/shark.jpg');
-new mallProduct('baby mop onesie', 'img/sweep.png');
-new mallProduct('tauntaun sleeping bag', 'img/tauntaun.jpg');
-new mallProduct('canned unicorn meat', 'img/unicorn.jpg');
-new mallProduct('tentacle usb drive', 'img/usb.gif');
-new mallProduct('useless water can', 'img/water-can.jpg');
-new mallProduct('useless wine glass', 'img/wine-glass.jpg');
-
-// console.log(mallProduct.allProducts);
+let products = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.jpg', 'water-can.jpg', 'wine-glass.jpg'];
 
 let threeProducts = document.getElementById('three-product');
 let leftProducts = document.getElementById('left-product');
 let centerProducts = document.getElementById('center-product');
 let rightProducts = document.getElementById('right-product');
 
+let votingTurns = 5;
+
+function mallProduct(name) {
+
+  this.name = name.substring(0, name.length - 4);
+  this.productShown = 0;
+  this.productClicked = 0;
+  this.image = `img/${name}`;
+
+  mallProduct.allImages.push(this);
+  mallProduct.imageMap[this.name] = this;
+}
+
+mallProduct.allImages = [];
+mallProduct.imageMap = {};
+
+for (let i = 0; i < products.length; i++) {
+  new mallProduct(products[i]);
+}
+
 function randomThree() {
-  let left = Math.floor(Math.random() * allProducts.length);
-  let center = Math.floor(Math.random() * allProducts.length);
-  let right = Math.floor(Math.random() * allProducts.length);
+  let left = Math.floor(Math.random() * products.length);
+  let center = Math.floor(Math.random() * products.length);
+  let right = Math.floor(Math.random() * products.length);
 
-  while ((left === center) && (left === right) && (right === center)) {
-    left = Math.floor(Math.random() * allProducts.length);
-    center = Math.floor(Math.random() * allProducts.length);
-    right = Math.floor(Math.random() * allProducts.length);
-  }
-
-  let leftOption = allProducts[left];
-  let centerOption = allProducts[center];
-  let rightOption = allProducts[right];
-
-  let returnArr = [leftOption, centerOption, rightOption];
-  console.log(returnArr)
-  return returnArr;
-}
-
-function renderProducts(leftOption, centerOption, rightOption) { // product shown never goes >1. how to make the system remember previous times shown. Push to an array? idk
-  leftProducts.src = leftOption.productPath;
-  leftOption.productShown++;
-
-  centerProducts.src = centerOption.productPath;
-  centerOption.productShown++;
-
-  rightProducts.src = rightOption.productPath;
-  rightOption.productShown++;
-}
-// console.log(allProducts)
-
-let randomProduct = randomThree();
-renderProducts(randomProduct[0], randomProduct[1], randomProduct[2]);
-
-threeProducts.addEventListener('click', function(event) {
-  console.log(event.target);
-
-  for (let i = 0; i < allProducts.length; i++) {
-    if (event.target.src.includes(allProducts[i].image)) {
-     allProducts[i].timesClicked++;
-      alert(allProducts[i]);
+  while (left === right || left === center || center ===right) {
+    if (left === right) {
+      right = Math.floor(Math.random() * products.length);
+    }
+    if (left === center) {
+      center = Math.floor(Math.random() * products.length);
+    }
+    if (center === right) {
+      right = Math.floor(Math.random() * products.length);
     }
   }
 
+  let leftOption = mallProduct.allImages[left];
+  let centerOption = mallProduct.allImages[center];
+  let rightOption = mallProduct.allImages[right];
+
+  return [leftOption, centerOption, rightOption];
+}
+
+function renderProducts() {
+
+  let productsDisplayed = [leftProducts.name, centerProducts.name, rightProducts.name];
+
   let threeMore = randomThree();
-  renderProducts(threeMore[0], threeMore[1], threeMore[2]);
-})
+  
+  while (
+    productsDisplayed[0] === threeMore[0].name ||
+    productsDisplayed[1] === threeMore[0].name ||
+    productsDisplayed[2] === threeMore[0].name ||
+    productsDisplayed[0] === threeMore[1].name ||
+    productsDisplayed[1] === threeMore[1].name ||
+    productsDisplayed[2] === threeMore[1].name ||
+    productsDisplayed[0] === threeMore[2].name ||
+    productsDisplayed[1] === threeMore[2].name ||
+    productsDisplayed[2] === threeMore[2].name
+  ) {
+    threeMore = randomThree();
+  }
+
+  leftProducts.src = threeMore[0].image;
+  leftProducts.name = threeMore[0].name;
+  threeMore[0].productShown++;
+
+  centerProducts.src = threeMore[1].image;
+  centerProducts.name = threeMore[1].name;
+  threeMore[1].productShown++;
+
+  rightProducts.src = threeMore[2].image;
+  rightProducts.name = threeMore[2].name;
+  threeMore[2].productShown++;
+}
+
+function handleClicks(event) {
+  votingTurns--;
+
+  for (let i = 0; i < mallProduct.allImages.length; i++) {
+    if (event.target.name === mallProduct.allImages[i].name) {
+      mallProduct.allImages[i].productClicked++;
+      console.log(mallProduct.allImages[i]);
+    }
+  }
+  renderProducts();
+
+  if (!votingTurns) {
+    threeProducts.removeEventListener('click', handleClicks);
+  }
+}
+
+renderProducts();
+threeProducts.addEventListener('click', handleClicks);
+
+// function randomThree() {
+
+//   let left = Math.floor(Math.random() * products.length);
+//   let center = Math.floor(Math.random() * products.length);
+//   let right = Math.floor(Math.random() * products.length);
+
+//   while ((left === center) && (left === right) && (right === center)) {
+//     left = Math.floor(Math.random() * products.length);
+//     center = Math.floor(Math.random() * products.length);
+//     right = Math.floor(Math.random() * products.length);
+//   }
+
+//   let leftOption = products[left];
+//   let centerOption = products[center];
+//   let rightOption = products[right];
+
+//   let returnArr = [leftOption, centerOption, rightOption];
+//   console.log(returnArr)
+//   return returnArr;
+// }
+
+// function renderProducts(leftOption, centerOption, rightOption) { // product shown never goes >1. how to make the system remember previous times shown. Push to an array? idk
+//   leftProducts.src = leftOption.productPath;
+//   leftOption.productShown++;
+
+//   centerProducts.src = centerOption.productPath;
+//   centerOption.productShown++;
+
+//   rightProducts.src = rightOption.productPath;
+//   rightOption.productShown++;
+// }
+// // console.log(products)
+
+// let randomProduct = randomThree();
+// renderProducts(randomProduct[0], randomProduct[1], randomProduct[2]);
+
+// threeProducts.addEventListener('click', function(event) {
+//   console.log(event.target);
+
+//   for (let i = 0; i < products.length; i++) {
+//     if (event.target.src.includes(products[i].image)) {
+//      products[i].timesClicked++;
+//       alert(products[i]);
+//     }
+//   }
+
+//   let threeMore = randomThree();
+//   renderProducts(threeMore[0], threeMore[1], threeMore[2]);
+// })
